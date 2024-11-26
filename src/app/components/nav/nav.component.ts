126e3542b6e66ed5e0d4d1d56639b7c793dbd7d7
Leandro Gonçalves <lleandrogon2004@gmail.com>
2024-11-26 13:30:51 -0300

    Ajustada geral no componente nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  visivel: boolean = false;
 
+  verMenu() {
+    this.visivel = !this.visivel;
+  }
 }
