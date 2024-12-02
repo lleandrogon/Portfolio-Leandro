d05aa4c5880773a7b33ac1e62cfc1127373e5126
Leandro Gonçalves <lleandrogon2004@gmail.com>
2024-12-02 11:48:41 -0300

    Adicionando opção de email e alterando design de alguns componentes

--- src/app/components/contatos/contatos.component.ts
+++ src/app/components/contatos/contatos.component.ts
@@ -17,19 +17,24 @@ export class ContatosComponent {
   });
 
   enviarWhatsapp() {
-    if (this.contatoForm.valid) {
       const nome = this.contatoForm.value.nome;
       const email = this.contatoForm.value.email;
       const assunto = this.contatoForm.value.mensagem;
 
       const numeroWhatsapp = "5582988094715";
-      const mensagemWhatsapp = `Nome: ${nome}\nemail: ${email}\n\nMensagem: ${assunto}`;
+      const mensagemWhatsapp = `Nome: ${nome}\nEmail: ${email}\n\nMensagem: ${assunto}`;
       const codificacao = encodeURIComponent(mensagemWhatsapp);
       const link = `https://wa.me/${numeroWhatsapp}?text=${codificacao}`;
 
       window.open(link, '_blank');
-    } else {
-      alert('Por favor preencha todos os campos!');
-    }
+  }
+
+  enviarEmail() {
+    const nome = this.contatoForm.value.nome;
+    const email = this.contatoForm.value.email;
+    const assunto = this.contatoForm.value.mensagem;
+
+    const emailLink = `mailto:lleandrogon2004@gmail.com?subject=Contato pelo Portfólio&body=Nome: ${nome}%0AEmail: ${email}%0AMensagem: ${assunto}`;
+    window.open(emailLink, '_blank');
   }
 }
